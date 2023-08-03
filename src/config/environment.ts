cb0b8f5866c1c60927e9d4d25144e226c4c2790b
ColorFilter <johan@romantech.net>
2023-08-04 02:41:38 +0900

    🔧 chore(.env.example): remove unused HOST variable from .env.example file 🔧 chore(environment.ts): simplify PORT assignment to use process.env.PORT or default to 3001 ✨ feat(server.ts): update server startup message to include the actual port number being used

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,3 +1,2 @@
 export const isProd = () => process.env.NODE_ENV === 'production';
-export const HOST = isProd() ? process.env.HOST : 'localhost';
-export const PORT = isProd() ? Number(process.env.PORT) : 3001;
+export const PORT = process.env.PORT || 3001;
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,15 @@
 import setupRoutes from './routes';
 import { createServer, errorHandler, notFoundHandler } from '@/config';
-import { HOST, isProd, PORT } from '@/config/environment';
+import { PORT } from '@/config/environment';
 
 const initServer = (): void => {
-  console.log('isProd', isProd());
-  console.log('port', PORT);
-  if (!PORT) {
-    console.error('Error: The PORT environment variable is not defined.');
-    process.exit(1);
-  }
-
   const app = createServer();
   setupRoutes(app);
 
   app.use(notFoundHandler);
   app.use(errorHandler);
 
-  app.listen(PORT, () => console.log(`Server is running at ${HOST}:${PORT}`));
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 };
 
 initServer();
