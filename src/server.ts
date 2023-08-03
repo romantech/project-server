e58f4e6dd508f34743f04a08cab5cf053f752919
ColorFilter <johan@romantech.net>
2023-08-04 02:04:59 +0900

    🔧 chore(server.ts): add console logs for isProd and port variables to aid in debugging 🐛 fix(server.ts): add error handling and exit process if PORT environment variable is not defined

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import setupRoutes from './routes';
 import { createServer, errorHandler, notFoundHandler } from '@/config';
-import { HOST, PORT } from '@/config/environment';
+import { HOST, isProd, PORT } from '@/config/environment';
 
 const initServer = (): void => {
+  console.log('isProd', isProd());
+  console.log('port', PORT);
   if (!PORT) {
     console.error('Error: The PORT environment variable is not defined.');
     process.exit(1);
