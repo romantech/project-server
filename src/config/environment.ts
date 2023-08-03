8d7fc6e56f70f024efb1beabf0a9aa6153d50ac2
ColorFilter <johan@romantech.net>
2023-08-04 04:27:47 +0900

    🔧 chore(environment.ts): update import statement for 'node:process' to resolve issue with unresolved variable in WebStorm due to tsconfig.json alias configuration 🐛 fix(environment.ts): update reference to process.env to use imported env from 'node:process' to resolve issue with unresolved variable in WebStorm due to tsconfig.json alias configuration ✨ feat(environment.ts): add support for using process.env.NODE_ENV and process.env.PORT environment variables to determine production mode and port respectively

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,2 +1,8 @@
-export const isProd = () => process.env.NODE_ENV === 'production';
-export const PORT = isProd() ? process.env.PORT : 3001;
+import { env } from 'node:process';
+
+/**
+ * 웹스톰에서 alias 설정을 위해 tsconfig.json 파일의 baseUrl 속성을 설정하면
+ * process.env 참조하면 해결되지 않은 변수로 나오는 문제 있음
+ * import { env } from 'node:process'; 구문으로 임포트해서 사용해서 임시 해결  */
+export const isProd = () => env.NODE_ENV === 'production';
+export const PORT = isProd() ? env.PORT : 3001;
