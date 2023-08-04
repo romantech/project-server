6754cd9f508746369d81a1206fb2ce457a0a0f4a
ColorFilter <johan@romantech.net>
2023-08-04 16:59:02 +0900

    📦 chore(server.ts): add comment explaining the usage of module-alias/register in deployment environment

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
 import setupRoutes from '@/routes';
 import { createServer, errorHandler, notFoundHandler, PORT } from '@/config';
 
+/**
+ * 배포 환경에선 빌드(ts -> js 파일로 트랜스파일) 후 모든 js 파일이 dist 폴더에 저장됨
+ * node -r module-alias/register 명령어는 module-alias/register 모듈을 실행하고,
+ * package.json._moduleAliases 속성을 참조해 @ 경로를 dist 폴더로 매핑함.
+ * 즉, 모든 import 구문의 @ 경로는 dist 폴더로 변환됨 e.g. @/config -> dist/config
+ * src/config 경로로 작성하면 dist 폴더에서 모듈을 찾는게 아니어서 에러 발생하므로 주의
+ * */
+
 const initServer = (): void => {
   const app = createServer();
   setupRoutes(app);
