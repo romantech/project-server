9362823780aab8ed13d4a506bf4973fd13c0e4a7
ColorFilter <johan@romantech.net>
2023-08-05 15:17:05 +0900

    📦 chore(package.json): add "@types/request-ip" dependency to improve type checking
    🚧 refactor(health.ts): use "request-ip" package to get client IP address instead of "req.ip" for better accuracy and compatibility

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express';
+import requestIP from 'request-ip';
 
 const router = Router();
 
 router.get(['/', '/health'], (req, res) => {
+  const client_ip = requestIP.getClientIp(req);
   const healthCheck = {
     uptime: process.uptime(),
     message: 'Server is up and running',
     timestamp: Date.now(),
     status: 'OK',
-    ip_address: req.ip,
+    client_ip,
   };
   res.send(healthCheck);
 });
