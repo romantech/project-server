1ce1e414d0b564fc7384cc53ec6fdcbafb4e956b
ColorFilter <johan@romantech.net>
2023-08-05 15:10:12 +0900

    🔧 fix(health.ts): include client IP address in health check response to provide additional information

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -2,12 +2,13 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get(['/', '/health'], (_req, res) => {
+router.get(['/', '/health'], (req, res) => {
   const healthCheck = {
     uptime: process.uptime(),
     message: 'Server is up and running',
     timestamp: Date.now(),
     status: 'OK',
+    ip_address: req.ip,
   };
   res.send(healthCheck);
 });
