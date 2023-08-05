81bb57e841097ad4d25b97b2d9b7c96a5a6e0c55
ColorFilter <johan@romantech.net>
2023-08-06 00:28:03 +0900

    🔧 fix(createAnalysis.ts): import ANALYSIS_REDIS_KEYS from redisKeys to fix undefined reference error 🔧 fix(createAnalysis.ts): add missing import statement for redisClient from db to fix undefined reference error 🔧 fix(createAnalysis.ts): add missing import statement for redis from db/redisClient to fix undefined reference error 🔧 fix(createAnalysis.ts): add missing import statement for ANALYSIS_REDIS_KEYS from utils/redisKeys to fix undefined reference error 🔧 fix(createAnalysis.ts): add missing import statement for TOTAL_COUNT from utils/redisKeys to fix undefined reference error 🔧 fix(createAnalysis.ts): add missing import statement for COUNT_BY_ID from utils/redisKeys to fix undefined reference error ✨ feat(createAnalysis.ts): decrement TOTAL_COUNT and COUNT_BY_ID in Redis by 1 or 3 based on the model used to improve accuracy of analysis count

--- src/controllers/analysis/createAnalysis.ts
+++ src/controllers/analysis/createAnalysis.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from 'express';
 import { asyncHandler } from '@/utils/asyncHandler';
-
+import redis from '@/db/redisClient';
 import { analysisPrompt } from '@/utils/analysisPrompt';
 import { throwCustomError } from '@/utils/customError';
 import { openai } from '@/config/openai';
 import { ERROR_MESSAGES } from '@/utils/errorMessages';
 import { body, validationResult } from 'express-validator';
+import { ANALYSIS_REDIS_KEYS } from '@/utils/redisKeys';
 
 type GPTModel = (typeof GPT_MODELS)[number];
 type RequestBody = {
@@ -16,6 +17,7 @@ type RequestBody = {
 
 const GPT_MODELS = ['gpt-3.5-turbo', 'gpt-4'];
 const { ANALYSIS_INVALID_REQUEST, ANALYSIS_INVALID_MODEL } = ERROR_MESSAGES;
+const { TOTAL_COUNT, COUNT_BY_ID } = ANALYSIS_REDIS_KEYS;
 
 const checkSentence = body('sentence')
   .isArray({ min: 2, max: 20 })
@@ -51,6 +53,10 @@ export const createAnalysis = [
     });
 
     if (!completion) throwCustomError('Error with OpenAI API request');
+
+    const decValue = model === 'gpt-4' ? 3 : 1;
+    await redis.decrby(TOTAL_COUNT, decValue);
+    await redis.decrby(COUNT_BY_ID(fingerprint), decValue);
     res.json({ result: completion.data.choices[0] });
   }),
 ];
