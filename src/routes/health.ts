153946580e74f746e1f2ef7fc23c049b9a6119ba
ColorFilter <johan@romantech.net>
2023-08-05 14:59:49 +0900

    📦 chore(package.json): add "@types/ip" and "ip" packages for better IP address handling 🔨 refactor(health.ts): add IP address to health check response for better monitoring and debugging

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
+import IP from 'ip';
 
 const router = Router();
 
-router.get(['/', '/health'], (_req, res) => {
+router.get(['/', '/health'], (req, res) => {
+  const ip = IP.address();
+
   const healthCheck = {
     uptime: process.uptime(),
     message: 'Server is up and running',
     timestamp: Date.now(),
     status: 'OK',
+    ip_address: ip,
   };
   res.send(healthCheck);
 });
