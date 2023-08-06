27b2b0a0d1ebb3845a47c998d210cca9f6e5c806
ColorFilter <johan@romantech.net>
2023-08-06 18:25:02 +0900

    🔧 chore(initRedisKeys.ts): remove unused constant EXP_PERIOD to improve code readability 🐛 fix(initRedisKeys.ts): remove expiration period when initializing TOTAL_COUNT key in Redis to prevent key from expiring

--- src/services/redis/initRedisKeys.ts
+++ src/services/redis/initRedisKeys.ts
@@ -2,7 +2,6 @@ import Redis from 'ioredis';
 import { ANALYSIS_REDIS_KEYS } from '@/services';
 
 export const INIT_TOTAL_COUNT = 50;
-const EXP_PERIOD = 24 * 60 * 60;
 
 export const initRedisKeys = async (redisClient: Redis) => {
   const { TOTAL_COUNT } = ANALYSIS_REDIS_KEYS;
@@ -10,7 +9,7 @@ export const initRedisKeys = async (redisClient: Redis) => {
     const totalCount = await redisClient.get(TOTAL_COUNT);
 
     if (totalCount === null) {
-      await redisClient.set(TOTAL_COUNT, INIT_TOTAL_COUNT, 'EX', EXP_PERIOD);
+      await redisClient.set(TOTAL_COUNT, INIT_TOTAL_COUNT);
     }
   };
 
