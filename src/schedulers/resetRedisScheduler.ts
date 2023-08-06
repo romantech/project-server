98a3a3c2349b9335415be01b856b01a3dc6a111a
ColorFilter <johan@romantech.net>
2023-08-06 18:54:53 +0900

    🔧 fix(resetRedisScheduler.ts): change console.log message to display the correct time for scheduled Redis total count reset

--- src/schedulers/resetRedisScheduler.ts
+++ src/schedulers/resetRedisScheduler.ts
@@ -6,14 +6,15 @@ import {
 import schedule from 'node-schedule';
 import { COLORS } from '@/constants';
 
-const { success } = COLORS;
-
 const resetAnalysisTotalCountKey = () => {
   redis.set(ANALYSIS_REDIS_KEYS.TOTAL_COUNT, ANALYSIS_TOTAL_INIT_COUNT);
 };
 
 export const scheduleRedisReset = () => {
-  // Koyeb 시간은 UTC 기준이므로 한국 시간(UTC+9) 24시는 '0 15 * * *'로 설정
+  // Koyeb 서버는 UTC 기준이므로 한국 시간(UTC+9) 00시는 '0 15 * * *'로 설정
   schedule.scheduleJob('0 15 * * *', resetAnalysisTotalCountKey);
-  console.log(success, 'Scheduled Redis total count reset at UTC 00:00 daily.');
+  console.log(
+    COLORS.success,
+    'Scheduled Redis total count reset at UTC 15:00 (KST 00:00) daily.',
+  );
 };
