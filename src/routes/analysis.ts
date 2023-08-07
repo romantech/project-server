d8584b122ab6aba4d6627dba3b348697e1e4779f
ColorFilter <johan@romantech.net>
2023-08-08 01:32:39 +0900

    🔥 refactor(analysis.ts): remove commented out code for getting remaining analysis count 🔀 merge(analysis.ts): update route path for getting remaining analysis count to improve clarity and consistency

--- src/routes/analysis.ts
+++ src/routes/analysis.ts
@@ -4,7 +4,6 @@ import { Router } from 'express';
 const analysisRouter = Router();
 
 analysisRouter.post('/', analysisController.createAnalysis); // 분석 데이터 생성
-// router.get('/remaining', syntaxAnalyzerController.getRemaining); // 금일 남은 요청 횟수
-analysisRouter.get('/remaining', analysisController.getRemainingCount); // 특정 사용자의  요청 남은 횟수
+analysisRouter.get('/remaining', analysisController.getRemainingCount); // 특정 사용자의 분석 요청 남은 횟수
 
 export { analysisRouter };
