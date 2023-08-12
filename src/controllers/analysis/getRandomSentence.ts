eba3ff05607bbd04268fe2ed50718223c2e158c9
ColorFilter <johan@romantech.net>
2023-08-12 17:15:43 +0900

    🔧 fix(getRandomSentence.ts): import throwCustomError function from utils to fix reference error 🔧 fix(getRandomSentence.ts): change getPromptTemplate function name to fetchPromptTemplate for clarity 🔧 fix(getRandomSentence.ts): change getSentenceFromOpenAI function name to fetchSentenceFromOpenAI for clarity ✨ feat(getRandomSentence.ts): add error handling for failed template retrieval and sentence generation from OpenAI service

--- src/controllers/analysis/getRandomSentence.ts
+++ src/controllers/analysis/getRandomSentence.ts
@@ -1,4 +1,4 @@
-import { asyncHandler } from '@/utils';
+import { asyncHandler, throwCustomError } from '@/utils';
 import { ParamsDictionary } from 'express-serve-static-core';
 import { ANALYSIS_REDIS_KEYS, openai, redis } from '@/services';
 import { MODEL_GPT_3_5, SENTENCE_QUERY_KEYS, SentenceQuery } from '@/constants';
@@ -20,7 +20,8 @@ export const getRandomSentence = [
     async (req, res) => {
       const { sent_count, topics, max_char } = req.query;
 
-      const template = await getPromptTemplate();
+      const template = await fetchPromptTemplate();
+      if (!template) return throwCustomError('Failed to get template', 500);
 
       const prompt = replaceTemplateValues(
         template,
@@ -29,16 +30,17 @@ export const getRandomSentence = [
         max_char,
       );
 
-      const completion = await getSentenceFromOpenAI(prompt);
-      const sentence = completion.data.choices?.[0]?.message?.content;
+      const sentence = await fetchSentenceFromOpenAI(prompt);
+      if (!sentence) throwCustomError('Failed to generate sentence', 500);
 
       res.status(200).json({ sentence });
     },
   ),
 ];
 
-const getPromptTemplate = async (): Promise<string> => {
-  return (await redis.get(ANALYSIS_REDIS_KEYS.PROMPT_SENTENCE)) as string;
+const fetchPromptTemplate = async () => {
+  const template = await redis.get(ANALYSIS_REDIS_KEYS.PROMPT_SENTENCE);
+  return template || null;
 };
 
 const replaceTemplateValues = (
@@ -53,9 +55,15 @@ const replaceTemplateValues = (
     .replace(`{${MAX_CHAR}}`, max);
 };
 
-const getSentenceFromOpenAI = async (prompt: string) => {
-  return await openai.createChatCompletion({
-    model: MODEL_GPT_3_5,
-    messages: [{ role: 'user', content: prompt }],
-  });
+const fetchSentenceFromOpenAI = async (prompt: string) => {
+  try {
+    const completion = await openai.createChatCompletion({
+      model: MODEL_GPT_3_5,
+      messages: [{ role: 'user', content: prompt }],
+    });
+
+    return completion.data.choices?.[0]?.message?.content || null;
+  } catch (error) {
+    throwCustomError('OpenAI service error', 500);
+  }
 };
