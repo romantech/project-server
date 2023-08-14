29f995c190105b29407b803442728e8c2f3d88cf
ColorFilter <johan@romantech.net>
2023-08-15 00:04:32 +0900

    🐛 fix(errorMessages.ts): update NOT_FOUND error message to include the original URL for better error identification 🐛 fix(notFoundHandler.ts): update notFoundHandler middleware to pass the originalUrl to the throwCustomError function

--- src/constants/errorMessages.ts
+++ src/constants/errorMessages.ts
@@ -1,8 +1,8 @@
 export const ERROR_MESSAGES = {
   SERVER_ERROR: 'Server Error.',
   IP_UNIDENTIFIABLE: 'Client IP address not found or unidentifiable.',
-  NOT_FOUND:
-    'The requested resource was not found. Please check your request URL and try again.',
+  NOT_FOUND: (originalUrl: string) =>
+    `The requested resource ${originalUrl} was not found. Please check your request URL and try again.`,
 
   MISSING_FIELD: (fieldName: string) => `${fieldName} field is required.`,
   RETRIEVE_FAILED: (resource: string) => `Failed to retrieve ${resource}.`,
--- src/middlewares/notFoundHandler.ts
+++ src/middlewares/notFoundHandler.ts
@@ -1,6 +1,11 @@
 import { throwCustomError } from '@/utils';
 import { ERROR_MESSAGES } from '@/constants';
+import { RequestHandler } from 'express';
 
-export const notFoundHandler = () => {
-  throwCustomError(ERROR_MESSAGES.NOT_FOUND, 404);
+export const notFoundHandler: RequestHandler = (
+  { originalUrl },
+  _res,
+  _next,
+) => {
+  throwCustomError(ERROR_MESSAGES.NOT_FOUND(originalUrl), 404);
 };
