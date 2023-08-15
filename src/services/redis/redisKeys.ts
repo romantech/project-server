d0df521a438b62f7dc3b190f2e6616bfcc738486
ColorFilter <johan@romantech.net>
2023-08-16 00:32:35 +0900

    🐛 fix(redisKeys.ts): change AnalyzerField enum name to AnalyzerFields for better naming consistency 🔀 chore(redisKeys.ts): update AnalyzerFieldType to use AnalyzerFields enum for improved type safety

--- src/services/redis/redisKeys.ts
+++ src/services/redis/redisKeys.ts
@@ -2,7 +2,7 @@ enum Namespace {
   ANALYZER = 'analyzer',
 }
 
-enum AnalyzerField {
+enum AnalyzerFields {
   ANALYSIS = 'analysis',
   RANDOM_SENTENCE = 'random_sentence',
 }
@@ -19,11 +19,11 @@ const ANALYZER_KEYS = {
 
 export const ANALYZER_REDIS_SCHEMA = {
   KEYS: ANALYZER_KEYS,
-  FIELDS: AnalyzerField, // 객체
+  FIELDS: AnalyzerFields, // 객체
 } as const;
 
 /**
  * 이넘(enum)은 런타임에선 객체로, 타입 문맥에선 멤버의 유니온 타입으로 동작.
  * 따라서, 아래 Record 키(key)로 사용된 이넘은 'analysis' | 'random_sentence' 타입.
  */
-export type AnalyzerFieldType = Record<AnalyzerField, string>;
+export type AnalyzerFieldType = Record<AnalyzerFields, string>;
