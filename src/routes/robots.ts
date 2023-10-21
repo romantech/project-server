9fca3641e566239c2e35ff7ed360192023607d4a
ColorFilter <johan@romantech.net>
2023-10-21 19:15:43 +0900

    📝 chore(robots.ts): update comments in robotsRouter.get() to provide more specific information about user-agents and disallowed paths

--- src/routes/robots.ts
+++ src/routes/robots.ts
@@ -5,8 +5,8 @@ const robotsRouter = Router();
 robotsRouter.get('/robots.txt', (req, res, _next) => {
   res.type('text/plain');
   /*
-   * User-agent: * (모든 웹 크롤러)
-   * Disallow: / (루트를 포함한 모든 경로에 대한 크롤러 비허용)
+   * User-agent: * (모든 웹 크롤러를 대상으로 함, 예: Googlebot, Bingbot 등)
+   * Disallow: / (루트 및 하위 경로에 대한 크롤러 접근 비허용)
    * */
   res.send('User-agent: *\nDisallow: /');
 });
